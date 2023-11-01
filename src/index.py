5e1971bd8f47047dd8e141efe06c40942dfe7547
Joseph Zahar <70657426+JosephZahar@users.noreply.github.com>
2023-11-01 11:13:28 -0400

    Update index.py server

--- src/index.py
+++ src/index.py
@@ -22,6 +22,7 @@ nav = navbar.Navbar()
 
 app = dash.Dash(__name__,meta_tags=[{"name": "viewport", "content": "width=device-width"}],
                 external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+server = app.server
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
